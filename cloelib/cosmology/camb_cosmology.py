da2de905526a9a8192f0474f91818bc0afcf31bf
zsirap <50758399+zsirap@users.noreply.github.com>
2025-06-09 08:29:07 +0300

    add h unit option to the volume element function

--- cloelib/cosmology/camb_cosmology.py
+++ cloelib/cosmology/camb_cosmology.py
@@ -186,7 +186,7 @@ class CAMBBackground:
         h_in_seconds = self.hubble_parameter(zs) / units.MPC_TO_KM
         return 3.0 * h_in_seconds**2.0 / (8.0 * np.pi * units.GRAVITATIONAL_CONSTANT)
 
-    def dV_dzdO(self, zs: np.ndarray) -> np.ndarray:
+    def dV_dzdO(self, zs: np.ndarray, hubble_units=False) -> np.ndarray:
         """
         Returns the volume element per redshit per solid angle
         at the redshift requested.
@@ -198,6 +198,13 @@ class CAMBBackground:
         Returns:
             np.ndarray: volume element in Mpc^3 h^{-3}
         """
+        if hubble_units == True:
+           return (
+            (units.SPEED_OF_LIGHT
+            / 1.0e3)
+            * self.comoving_distance(zs) ** 2.0
+            / self.hubble_parameter(zs)
+        ) * (self.H0/100.0)**3.0     
         return (
             (units.SPEED_OF_LIGHT
             / 1.0e3)
