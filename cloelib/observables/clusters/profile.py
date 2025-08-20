f8a77154609c5c3c84907d0fedc822b215ac7046
Michel <aguena@lapp.in2p3.fr>
2025-08-20 14:31:11 +0200

    mv cloelib/tests/cluster/ -> cloelib/tests_cluster/

--- cloelib/observables/clusters/profile.py
+++ cloelib/observables/clusters/profile.py
@@ -1,7 +1,6 @@
 import numpy as np
 from scipy.stats import skewnorm
 from scipy.integrate import simpson as simps
-from astropy import constants as ap_constants
 from astropy import units as ap_units
 from scipy import interpolate
 from scipy.integrate import quad_vec
