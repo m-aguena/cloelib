f9a9da80d9db12680fae81085435b91ef6c12cbc
Michel <aguena@lapp.in2p3.fr>
2025-08-20 14:50:17 +0200

    fix docstring

--- cloelib/cosmology/camb_cosmology.py
+++ cloelib/cosmology/camb_cosmology.py
@@ -130,7 +130,7 @@ class CAMBBackground:
 
     def Omega_m_cb(self, zs: np.ndarray) -> np.ndarray:
         """
-        Returns the matter density (no neutrinos) as a function of redshift.
+        Return the matter density (no neutrinos) as a function of redshift.
 
         Args:
             zs (np.ndarray): Array of redshifts.
@@ -144,7 +144,7 @@ class CAMBBackground:
 
     def Omega_m(self, zs: np.ndarray) -> np.ndarray:
         """
-        Returns the matter density as a function of redshift.
+        Return the matter density as a function of redshift.
 
         Args:
             zs (np.ndarray): Array of redshifts.
