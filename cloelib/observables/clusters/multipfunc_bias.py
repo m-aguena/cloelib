13e9341a83cce7a19b8aa59ebb8ecc1eb992dbb1
zsirap <50758399+zsirap@users.noreply.github.com>
2025-09-16 13:20:03 +0300

    Update multipfunc_bias.py

--- cloelib/observables/clusters/multipfunc_bias.py
+++ cloelib/observables/clusters/multipfunc_bias.py
@@ -5,13 +5,7 @@ import numpy as np  # type: ignore
 import jax.numpy as jnp
 
 """
-## Notes:
- 
-- Refactored cosmology.py from the original CLOE to provide a more flexible framework,
-  enabling seamless integration with external cosmological codes while removing dependency on Cobaya.
-
-- Introduced the use of protocols to standardize external code interfaces,
-  providing a unified and extensible template for interaction.
+- Introducing a protocol for the halo statistics part that we might have many versions of it.
 """
 
 T = TypeVar("T", bound=Union[jnp.ndarray, np.ndarray])
