036c2d10f5c9e96b29b9d8644702a775c9b4a075
zsirap <50758399+zsirap@users.noreply.github.com>
2025-09-17 17:27:09 +0300

    Update derived_cosmology.py

--- cloelib/cosmology/derived_cosmology.py
+++ cloelib/cosmology/derived_cosmology.py
@@ -38,10 +38,12 @@ def dV_dzdO(background, zs: np.ndarray, hubble_units=False) -> np.ndarray:
         Background class containing cosmology
     zs :np.ndarray
         Redshifts.
+    hubble_units: (Optional) bool
+        Flag to specify if output in h units, defaults to False
 
     Returns
     -------
-        np.ndarray: volume element in Mpc^3 h^{-3}
+        np.ndarray: volume element in Mpc^3 (h^{-3})
     """
     _dV_dzdO = (
         units.SPEED_OF_LIGHT
