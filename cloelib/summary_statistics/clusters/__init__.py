83f03211ccce1514befdc5162044bbf33a09b401
Michel Aguena <aguena@inaf.it>
2025-11-26 11:51:48 +0100

    rename cloelib/summary_statistics/clusters/profiles.py -> cloelib/summary_statistics/clusters/lensing.py

--- cloelib/summary_statistics/clusters/__init__.py
+++ cloelib/summary_statistics/clusters/__init__.py
@@ -1,4 +1,4 @@
 from .clustering import ClusterClustering
 from .counts import ClusterCounts
-from .profiles import ClusterWeakLensing
+from .lensing import ClusterWeakLensing
 from .statistics_modeling import ClusterStatisticsModeling
