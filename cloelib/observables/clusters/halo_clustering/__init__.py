82e222780ccf602fb988849ebce5414449dbf8c9
Michel <aguena@lapp.in2p3.fr>
2026-04-29 12:02:30 +0200

    add __all__

--- cloelib/observables/clusters/halo_clustering/__init__.py
+++ cloelib/observables/clusters/halo_clustering/__init__.py
@@ -1,2 +1,7 @@
 from .halo_clustering import HaloClustering
 from .twopoint3d_halo_clustering import TwoPoint3DHaloClustering
+
+__all__ = [
+    "HaloClustering",
+    "TwoPoint3DHaloClustering",
+]
--- cloelib/observables/clusters/halo_profile/__init__.py
+++ cloelib/observables/clusters/halo_profile/__init__.py
@@ -2,3 +2,11 @@ from .bmo_halo_profile import BMOHaloProfile
 from .halo_profile import HaloProfile
 from .halo_profile_core import HaloProfileCore
 from .nfw_halo_profile import NFWHaloProfile
+
+
+__all__ = [
+    "BMOHaloProfile",
+    "HaloProfile",
+    "HaloProfileCore",
+    "NFWHaloProfile",
+]
