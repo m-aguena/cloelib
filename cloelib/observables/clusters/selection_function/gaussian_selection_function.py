cdf2700929f41f6d9f7fc5131f7a699bad6728bc
GiorgioLesci <lesci.giorgio@gmail.com>
2026-06-05 13:43:57 +0200

    removed unused input parameter

--- cloelib/observables/clusters/selection_function/gaussian_selection_function.py
+++ cloelib/observables/clusters/selection_function/gaussian_selection_function.py
@@ -3,15 +3,10 @@
 import numpy as np
 from scipy.integrate import simpson
 
-from cloelib.observables.clusters.halo_mass_observable import (
-    HaloMassObservable,
-)
-
 
 class GaussianSelectionFunction:
     def __init__(
         self,
-        halo_mass_observable: HaloMassObservable,
         sig_lambda_norm: float,
         sig_lambda_z: float,
         sig_lambda_exponent: float,
@@ -27,8 +22,6 @@ class GaussianSelectionFunction:
 
         Parameters
         ----------
-        halo_mass_observable: HaloMassObservable,
-            Object that contains the distribution of true richness given mass
         sig_lambda_norm: float
             Amplitude of the observed proxy - true proxy relation
         sig_lambda_z: float
