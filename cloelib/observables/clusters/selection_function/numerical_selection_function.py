305e0b10f266ec09e6eae28b4a6ab992db64247f
Michel Aguena <aguena@inaf.it>
2026-07-28 10:17:01 +0200

    run pre-commit

--- cloelib/observables/clusters/selection_function/numerical_selection_function.py
+++ cloelib/observables/clusters/selection_function/numerical_selection_function.py
@@ -360,7 +360,7 @@ class NumericalSelectionFunction:
         lambda_obs_edges : numpy.ndarray
             Edges of richness bins for the integration.
         mass : numpy.ndarray
-            Mass to compute the window. 
+            Mass to compute the window.
             Note: to keep the order of the parameters consistent
             with other window_redshift_richness_observed mass is
             set to None, even if the value of the parameter is needed by the function
